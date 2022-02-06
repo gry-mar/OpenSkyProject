561ac8e2921747c0f542be4833b38d99ef204e60
gry-mar <254673@student.pwr.edu.pl>
2022-02-06 19:14:02 +0100

    idk

--- app/src/main/java/edu/ib/openskyproject/MapsActivity.java
+++ app/src/main/java/edu/ib/openskyproject/MapsActivity.java
@@ -15,9 +15,15 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -36,6 +42,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
 
 import edu.ib.openskyproject.databinding.ActivityMapsBinding;
 
@@ -110,6 +120,87 @@ public class MapsActivity extends FragmentActivity {
     public void returnMain(View view) {
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
+    }
+
+   public void showFlightsNearby(View view) {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+//                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+//        }else{
+//            Task<Location> task = fusedLocationProviderClient.getLastLocation();
+//            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//                @Override
+//                public void onSuccess(Location location) {
+//                    if (location != null) {
+//                        LatLng latLng;
+//                        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                        EditText areaText = findViewById(R.id.edtArea);
+//                        double areaKm = Double.parseDouble(areaText.toString());
+//                        double degrees = distanceToLatAndLong(areaKm);
+//                        String url = "https://opensky-network.org/api/states/all?lamin=" + String.valueOf(location.getLatitude() - degrees) +
+//                                "&lomin=" + String.valueOf(location.getLongitude() - degrees) + "&lamax=" +
+//                                String.valueOf(location.getLatitude() + degrees) + "&lomax=" + String.valueOf(location.getLongitude() + degrees);
+//                        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//                        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//                        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                                res -> {
+//                                    Results results = gson.fromJson(res.toString(), Results.class);
+//
+//                                    ArrayList<Flights> flightsArrayList = new ArrayList<>();
+//                                    ArrayList<ArrayList> statesList = new ArrayList<>();
+//                                    statesList.addAll(results.getStates());
+//                                    ArrayList<String> countriesList = new ArrayList<>();
+//                                    for (int i = 0; i < statesList.size(); i++) {
+//                                        try {
+//                                            double latitude = Double.parseDouble(statesList.get(i).get(6).toString());
+//                                            double longitude = Double.parseDouble(statesList.get(i).get(5).toString());
+//                                            String country = statesList.get(i).get(2).toString();
+//                                            flightsArrayList.add(new Flights(longitude, latitude));
+//                                            countriesList.add(country);
+//                                        } catch (NullPointerException e) {
+//                                            e.printStackTrace();
+//                                        }
+//
+//                                    }
+//                                    for (int i = 0; i < flightsArrayList.size(); i++) {
+//                                        LatLng point = new LatLng(flightsArrayList.get(i).getLatitude(),
+//                                                flightsArrayList.get(i).getLongitude());
+//                                        MarkerOptions markerOptions = new MarkerOptions().position(point).title("Country: " +
+//                                                countriesList.get(i));
+//                                        mMap.addMarker(markerOptions);
+//
+//                                    }
+//                                },
+//                                error -> {
+//                                    System.out.println("Error");
+//                                });
+//                        queue.add(stringRequest);
+//
+//
+//                    }
+//
+//                }
+//
+//                ;
+//
+//            });
+//                        }
+                    }
+
+
+    private double distanceToLatAndLong(double distance){
+        int oneDegreeVal = 111;
+        int oneMinVal = 2;
+        if(distance % oneDegreeVal ==0){
+            return distance/oneDegreeVal;
+        }else{
+            double r = distance % oneDegreeVal;
+            double d = (distance - r)/111;
+            double m = r/oneMinVal;
+            return d+m;
+        }
     }
 
     //    @Override
