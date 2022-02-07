91ea72c49b97b4dedb54edcf4c00aea6a3b4528d
eeyore7 <hubert.romaniak@wp.pl>
2022-02-07 21:37:11 +0100

    10001011001101011010

--- app/src/main/java/edu/ib/openskyproject/ShowPlaneMapFragment.java
+++ app/src/main/java/edu/ib/openskyproject/ShowPlaneMapFragment.java
@@ -3,13 +3,11 @@ package edu.ib.openskyproject;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
