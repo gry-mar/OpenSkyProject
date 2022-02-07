8710edcd1479297068d976d9498a16d0a093e7e3
gry-mar <254673@student.pwr.edu.pl>
2022-02-07 19:53:04 +0100

    gggg

--- app/src/main/java/edu/ib/openskyproject/MainActivity.java
+++ app/src/main/java/edu/ib/openskyproject/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void trackFlightsClicked(View view) {
-
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
