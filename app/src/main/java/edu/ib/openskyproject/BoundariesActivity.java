cd5a54d0fc083de75b2f2fef681ea1805242be8c
eeyore7 <hubert.romaniak@wp.pl>
2022-02-09 15:45:19 +0100

    wazaaaaaa

--- app/src/main/java/edu/ib/openskyproject/BoundariesActivity.java
+++ app/src/main/java/edu/ib/openskyproject/BoundariesActivity.java
@@ -1,8 +1,6 @@
 package edu.ib.openskyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
